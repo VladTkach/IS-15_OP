c3e3a107cf458ebfb8b3b9fffd2f9f09fb42fd86
VladTkach <70843124+VladTkach@users.noreply.github.com>
2021-09-23 10:33:28 +0300

    Add files via upload

---
+++ 31.py
@@ -0,0 +1,17 @@
+import math
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+print(a, b, c)
+
+p = (a + b + c) / 2
+S = math.sqrt(p * (p - a) * (p - b) * (p - c))
+R = a * b * c / 4 / S
+r = 2 * S / (a + b + c)
+bi1 = math.sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b)
+bi2 = math.sqrt(b * c * (b + c + a) * (b + c - a)) / (b + c)
+bi3 = math.sqrt(a * c * (a + c + b) * (a + c - b)) / (a + c)
+
+print(str(bi1) + "\n" + str(bi2) + "\n" + str(bi3) + "\n" + str(r) + "\n" + str(R))
