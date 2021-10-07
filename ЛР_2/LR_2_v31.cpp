18533ab3a835bd86aa8896c9e94d5e22b37e97d9
VladTkach <70843124+VladTkach@users.noreply.github.com>
2021-10-07 10:33:08 +0300

    Add files via upload

---
+++ ЛР_2/LR_2_v31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float a, b, c;
+	cin >> a >> b >> c;
+	if (a == b || a == c || b == c){
+		cout<<"YES";
+	}
+	else {
+		cout<<"NO";
+	}
+}
---
+++ ЛР_2/LR_2_v31.py
@@ -0,0 +1,8 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+if a == b or a == c or b == c:
+    print("YES ")
+else:
+    print("NO")
